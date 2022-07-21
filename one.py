691b00f7fb9eac2e7517ad783aa63a55544deba9
OmkarGitPatil <omkarlpatil64@gmail.com>
2022-07-21 07:22:49 +0530

    added predict func to one.py and added display.html

--- one.py
+++ one.py
@@ -6,5 +6,9 @@ app=Flask(__name__)
 def basic():
     return 'Basic URL Created'
 
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    return 'This is predict function'
+
 if __name__=='__main__':
     app.run(debug=True)
